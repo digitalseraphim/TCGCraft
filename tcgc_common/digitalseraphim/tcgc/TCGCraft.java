18a79b2861bd17a053c40edc959d1453033e1ffb
Nick Owens <digitalseraphim@gmail.com>
2013-12-13 00:32:21 -0500

    commit @ Fri Dec 13 00:32:21 EST 2013 [ starting! ]

--- tcgc_common/digitalseraphim/tcgc/TCGCraft.java
+++ tcgc_common/digitalseraphim/tcgc/TCGCraft.java
@@ -37,5 +37,4 @@ public class TCGCraft {
 		proxy.initItems();
 	}
 	
-	
 }
---
+++ tcgc_common/digitalseraphim/tcgc/blocks/BlockCardTable.java
@@ -0,0 +1,10 @@
+package digitalseraphim.tcgc.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockCardTable extends Block {
+	public BlockCardTable(int id) {
+		super(id, Material.rock);
+	}
+}
