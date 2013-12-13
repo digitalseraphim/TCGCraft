02d695ee491599690fcf8f7f7b41031973b2bfda
Nick Owens <digitalseraphim@gmail.com>
2013-12-13 00:47:24 -0500

    commit @ Fri Dec 13 00:47:24 EST 2013 [ starting! ]

--- tcgc_common/digitalseraphim/tcgc/blocks/BlockCardTable.java
+++ tcgc_common/digitalseraphim/tcgc/blocks/BlockCardTable.java
@@ -1,10 +1,30 @@
 package digitalseraphim.tcgc.blocks;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.util.Icon;
 
 public class BlockCardTable extends Block {
+	Icon sideIcon;
+	
 	public BlockCardTable(int id) {
 		super(id, Material.rock);
+	}
+	
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public Icon getIcon(int par1, int par2) {
+		return sideIcon;
+	}
+	
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister iconRegister) {
+		sideIcon = iconRegister.registerIcon("tcgc:cardtable_block.png");
 	}
 }
