62af5eb822afa3f2f05f4cacb06b148e39742856
Nick Owens <digitalseraphim@gmail.com>
2013-12-13 19:50:22 -0500

    commit @ Fri Dec 13 19:50:22 EST 2013 [ back to modding! ]

--- tcgc_common/digitalseraphim/tcgc/client/render/items/IIRCard.java
+++ tcgc_common/digitalseraphim/tcgc/client/render/items/IIRCard.java
@@ -3,13 +3,10 @@ package digitalseraphim.tcgc.client.render.items;
 import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.entity.EntityLiving;
-import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.storage.MapData;
 import net.minecraftforge.client.IItemRenderer;
-
-import org.lwjgl.opengl.GL11;
-
 import digitalseraphim.tcgc.items.ItemCard;
 
 public class IIRCard implements IItemRenderer {
@@ -30,27 +27,33 @@ public class IIRCard implements IItemRenderer {
 		switch (type) {
 		case ENTITY: {
 			RenderBlocks render = (RenderBlocks) data[0];
-			EntityItem entity = (EntityItem) data[1];
+//			EntityItem entity = (EntityItem) data[1];
+			
+			renderCardBack(render);
 		}
 			break;
 		case EQUIPPED: {
 			RenderBlocks render = (RenderBlocks) data[0];
 			EntityLiving entity = (EntityLiving) data[1];
+			renderCardBack(render);
 		}
 			break;
 		case EQUIPPED_FIRST_PERSON: {
 			RenderBlocks render = (RenderBlocks) data[0];
 			EntityLiving entity = (EntityLiving) data[1];
+			System.out.println("IIRCard.renderItem() - EQUIPPED_FIRST_PERSON");
 		}
 			break;
 		case FIRST_PERSON_MAP: {
 			RenderBlocks render = (RenderBlocks) data[0];
 			EntityLiving entity = (EntityLiving) data[1];
 			MapData mapData = (MapData) data[2];
+			renderCardBack(render);
 		}
 			break;
 		case INVENTORY: {
 			RenderBlocks render = (RenderBlocks) data[0];
+			renderCardBack(render);
 		}
 			break;
 		default:
@@ -66,8 +69,21 @@ public class IIRCard implements IItemRenderer {
 
 	public void renderCardBack(RenderBlocks render){
 		Tessellator tess = Tessellator.instance;
-		render.
-		tess.
+		bindTexture(render, new ResourceLocation("tcgc:textures/items/card_back"));
+		float x = 1.F/32.F;
 		
+		tess.startDrawingQuads();
+		
+		tess.addVertexWithUV(-12*x, .1, -.5, 0, 1);
+		tess.addVertexWithUV(-12*x, .1, .5, 0, 0);
+		tess.addVertexWithUV(12*x, .1, .5, 1, 1);
+		tess.addVertexWithUV(12*x, .1, -.5, 1, 0);
+		
+		tess.draw();
 	}
+	
+    protected void bindTexture(RenderBlocks render, ResourceLocation res)
+    {
+    	render.minecraftRB.getTextureManager().bindTexture(res);
+    }
 }
