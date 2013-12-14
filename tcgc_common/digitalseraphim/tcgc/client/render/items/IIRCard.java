d318fa24ec2381420727902a71dbd021c2308f78
Nick Owens <digitalseraphim@gmail.com>
2013-12-13 21:50:47 -0500

    commit @ Fri Dec 13 21:50:47 EST 2013 [ back to modding! ]

--- tcgc_common/digitalseraphim/tcgc/client/render/items/IIRCard.java
+++ tcgc_common/digitalseraphim/tcgc/client/render/items/IIRCard.java
@@ -70,6 +70,7 @@ public class IIRCard implements IItemRenderer {
 	float partialTicks = 0;
 	public void renderFirstPerson(EntityPlayer player, RenderBlocks render) {
 		GL11.glPopMatrix();
+		GL11.glPopMatrix();
 		GL11.glPushMatrix();
 		partialTicks=5;
 		float f12 = 0.8F;
@@ -137,6 +138,7 @@ public class IIRCard implements IItemRenderer {
 		tessellator.addVertexWithUV((double) (0 - b0), (double) (0 - b0), 0.0D, 0.0D, 0.0D);
 		tessellator.draw();
 		GL11.glPopMatrix();
+		GL11.glPushMatrix();
 		GL11.glPushMatrix();
 
 	}
