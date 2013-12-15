a308d3f14a9bfc9889fb11f92042497c75a8bc7f
Nick Owens <digitalseraphim@gmail.com>
2013-12-15 12:53:38 -0500

    commit @ Sun Dec 15 12:53:39 EST 2013 [ sleep ]

---
+++ tcgc_common/digitalseraphim/tcgc/core/logic/EntityFireBallTest.java
@@ -0,0 +1,20 @@
+package digitalseraphim.tcgc.core.logic;
+
+import net.minecraft.entity.projectile.EntityFireball;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.world.World;
+
+public class EntityFireBallTest extends EntityFireball {
+
+	public EntityFireBallTest(World par1World) {
+		super(par1World);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onImpact(MovingObjectPosition movingobjectposition) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
